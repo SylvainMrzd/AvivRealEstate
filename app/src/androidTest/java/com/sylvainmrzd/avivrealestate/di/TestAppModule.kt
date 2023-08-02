40165107ae09b450ef8aa5c7e77d6b71c52d1626
Sylvain Merzaud <sylvain.merzaud@lagencesentinel.com>
2023-08-02 14:51:54 +0200

    UI tests implementation

---
+++ app/src/androidTest/java/com/sylvainmrzd/avivrealestate/di/TestAppModule.kt
@@ -0,0 +1,36 @@
+package com.sylvainmrzd.avivrealestate.di
+
+import com.sylvainmrzd.avivrealestate.feature_realestate_ads.data.data_source.AvivApi
+import com.sylvainmrzd.avivrealestate.feature_realestate_ads.data.data_source.MockInterceptor
+import com.sylvainmrzd.avivrealestate.feature_realestate_ads.data.repository.AvivRealEstateRepository
+import com.sylvainmrzd.avivrealestate.feature_realestate_ads.data.repository.DefaultAvivRealEstateRepository
+import com.sylvainmrzd.avivrealestate.others.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestAppModule {
+
+    @Singleton
+    @Provides
+    fun provideAvivApi(): AvivApi {
+        val client = OkHttpClient.Builder().addInterceptor(MockInterceptor()).build()
+
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build().create(AvivApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideDefaultAvivRepository(api: AvivApi) = DefaultAvivRealEstateRepository(api) as AvivRealEstateRepository
+}
---
+++ app/src/androidTest/java/com/sylvainmrzd/avivrealestate/feature_realestate_ads/presentation/realestate_ads_list/view/AdsListScreenTest.kt
@@ -0,0 +1,103 @@
+package com.sylvainmrzd.avivrealestate.feature_realestate_ads.presentation.realestate_ads_list.view
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollToIndex
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.test.swipeDown
+import androidx.compose.ui.test.swipeUp
+import com.sylvainmrzd.avivrealestate.di.AppModule
+import com.sylvainmrzd.avivrealestate.feature_realestate_ads.presentation.MainActivity
+import com.sylvainmrzd.avivrealestate.others.Constants
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.util.Timer
+import kotlin.concurrent.schedule
+
+@HiltAndroidTest
+@UninstallModules(AppModule::class)
+class AdsListScreenTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @get:Rule(order = 2)
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun scrollAdsListUpAndDown() {
+        composeRule.waitUntilTimeout(10_000)
+        composeRule.onNodeWithTag(Constants.ADS_LIST_TAG).performTouchInput { swipeUp() }
+        composeRule.onNodeWithTag(Constants.ADS_LIST_TAG).performTouchInput { swipeDown() }
+        composeRule.onNodeWithTag(Constants.ADS_LIST_TAG).performTouchInput { swipeUp() }
+        composeRule.onNodeWithTag(Constants.ADS_LIST_TAG).performTouchInput { swipeDown() }
+        composeRule.onNodeWithTag(Constants.ADS_LIST_TAG).performTouchInput { swipeUp() }
+        composeRule.onNodeWithTag(Constants.ADS_LIST_TAG).performTouchInput { swipeDown() }
+    }
+
+    @Test
+    fun list_contains_all_elements() {
+        composeRule.waitUntilTimeout(10_000)
+
+        for (i in 0..7) {
+            composeRule.onNodeWithTag(Constants.ADS_LIST_TAG).performScrollToIndex(i)
+            composeRule.onNodeWithTag(Constants.ADS_ITEM_TAG.plus(i)).assertExists()
+        }
+    }
+
+    @Test
+    fun all_elements_are_displayed() {
+        composeRule.waitUntilTimeout(10_000)
+
+        for (i in 0..7) {
+            composeRule.onNodeWithTag(Constants.ADS_LIST_TAG).performScrollToIndex(i)
+            composeRule.onNodeWithTag(Constants.ADS_ITEM_TAG.plus(i)).assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun click_on_list_element() {
+        composeRule.waitUntilTimeout(10_000)
+
+        for (i in 0..7) {
+            composeRule.onNodeWithTag(Constants.ADS_LIST_TAG).performScrollToIndex(i)
+            composeRule.onNodeWithTag(Constants.ADS_ITEM_TAG.plus(i)).performClick()
+        }
+    }
+
+    fun ComposeContentTestRule.waitUntilTimeout(
+    timeoutMillis: Long
+    ) {
+        AsyncTimer.start(timeoutMillis)
+        this.waitUntil(
+            condition = { AsyncTimer.expired },
+            timeoutMillis = timeoutMillis + 1000
+        )
+    }
+
+    object AsyncTimer {
+        var expired = false
+        fun start(delay: Long = 1000) {
+            expired = false
+            Timer().schedule(delay) {
+                expired = true
+            }
+        }
+    }
+}
